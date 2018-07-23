42c8b067ace4c1f067c725aa0b46de19cb066db4
corey decato <corey.decato@richmond.edu>
2018-07-23 14:48:10 -0400

    final push for submitting

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Urban Burger');
+INSERT INTO burgers (name) VALUES ('Loaded Burger', true);
+INSERT INTO burgers (name) VALUES ('Veggie Burger');
